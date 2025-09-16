94733e52c3b577adb462eca784f52e8bdb29300f
Faizan <faizanyaseen17@gmail.com>
2025-09-16 14:25:41 +0500

    Fix: Remove sign up page
    Signed-off-by: Faizan <faizanyaseen17@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users, skip: [:registrations], controllers: {
+  registrations: 'users/registrations'
+}
+
+devise_scope :user do
+  resource :registration,
+    only: [:edit, :update, :destroy],
+    path: 'users',
+    path_names: { new: 'sign_up' },
+    controller: 'users/registrations',
+    as: :user_registration
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
